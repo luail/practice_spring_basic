2a25e9b003a0bb82f789692128420f0dc1badd25
luail <kynia0506@naver.com>
2025-01-09 17:06:53 +0900

    first

---
+++ src/test/java/com/practiceSpring/springBasicPractice/SpringBasicPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practiceSpring.springBasicPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
