1c63f627a543d19924a4bd9fd77c34513e2fe273
luail <kynia0506@naver.com>
2025-01-10 23:39:42 +0900

    01.10 spring practice

--- src/main/java/com/practiceSpring/springBasicPractice/controller/HelloController.java
+++ src/main/java/com/practiceSpring/springBasicPractice/controller/HelloController.java
@@ -6,12 +6,15 @@ import com.practiceSpring.springBasicPractice.domain.Hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 //Component 어노테이션을 통해 별도의 객체를 생성할 필요가 없는 싱글톤 객체를 생성.
 //Contoller 어노테이션을 통해 쉽게 사용자의 http요청을 처리하고 http응답을 줄 수 있음.
 @Controller
 //클래스 차원에 url매핑시에는 RequestMapping을 사용한다.
-@RequestMapping("/hello")
+@RequestMapping("/practice")
 public class HelloController {
 
 //    case1. 서버가 사용자에게 단순 String 데이터 return
@@ -93,6 +96,92 @@ public class HelloController {
     public String path(@PathVariable String inputName, Model model) {
         model.addAttribute("modelName",inputName);
         return "path";
+    }
+
+
+    @GetMapping("/form-view")
+    public String formView() {
+        return "form-view";
+    }
+//    case1. form데이터 형식의 post요청처리(텍스트만 있는 application/x-www~)
+//    형식 : ?name=xxx&email=yyy 데이터가 http body에 들어옴.
+    @PostMapping("/form-view")
+    @ResponseBody
+    public String formViewPost(Hello hello) {
+        System.out.println(hello);
+        return "OK";
+    }
+
+//    case2. form데이터 형식의 post요청 처리(file + text 형식)
+//    url패턴 : form-file-view
+    @GetMapping("/form-file-view")
+    public String formFileView() {
+        return "form-file-view";
+    }
+
+    @PostMapping("/form-file-view")
+    @ResponseBody
+    public String formFileViewPost(Hello hello, @RequestParam(value = "photo")MultipartFile photo) {
+        System.out.println(hello);
+        System.out.println(photo.getOriginalFilename());
+        return "OK";
+    }
+
+//    case3. js를 활용한 form데이터 전송(text만)
+    @GetMapping("axios-form-view")
+    public String axiosFormView() {
+        return "axios-form-view";
+    }
+
+    @PostMapping("/axios-form-view")
+    @ResponseBody
+    public String axiosFormViewPost(Hello hello) {
+        System.out.println(hello);
+        return "OK";
+    }
+
+//    case4. js를 활용한 form데이터 전송(text + file)
+    @GetMapping("/axios-form-file-view")
+    public String axiosFormFileView() {
+        return "axios-form-file-view";
+    }
+
+    @PostMapping("/axios-form-file-view")
+    @ResponseBody
+    public String axiosFormFileViewPost(Hello hello, @RequestParam(value = "photo")MultipartFile photo) {
+        System.out.println(hello);
+        System.out.println(photo.getOriginalFilename());
+        return "OK";
+    }
+
+//    case5. js를 활용한 form데이터 전송(text + 멀티file)
+    @GetMapping("/axios-form-multi-file-view")
+    public String axiosFormMultiFileView () {
+        return "axios-form-multi-file-view";
+    }
+
+    @PostMapping("/axios-form-multi-file-view")
+    @ResponseBody
+    public String axiosFormMultiFileViewPost(Hello hello, @RequestParam(value = "photos") List<MultipartFile> photos) {
+        System.out.println(hello);
+        for (int i = 0; i < photos.size(); i++) {
+            System.out.println(photos.get(i).getOriginalFilename());
+        }
+        return "OK";
+    }
+
+//    case6. js를 활용한 json데이터 전송
+//    형식 : {name:"hong", email:"hong@naver.com}
+    @GetMapping("/axios-json-view")
+    public String axiosJsonView() {
+        return "axios-json-view";
+    }
+
+    @PostMapping("/axios-json-view")
+    @ResponseBody
+    public String axiosJsonViewPost(@RequestBody Hello hello) {
+        System.out.println(hello);
+        return "OK";
     }
 }
 
